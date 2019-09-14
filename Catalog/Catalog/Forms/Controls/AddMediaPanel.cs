6f5fa8631356b5b0e6aae17a7208fcda302271f8
Amir Grozki <amirgrozki@gmail.com>
2019-09-14 20:33:13 +0300

    Added a panel for adding media.

---
+++ Catalog/Catalog/Forms/Controls/AddMediaPanel.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Catalog.Model;
+using Eto.Drawing;
+using Eto.Forms;
+
+namespace Catalog.Forms
+{
+    public class AddMediaPanel : Panel
+    {
+        private Dictionary<MediaType, NumericStepper> steppers = new Dictionary<MediaType, NumericStepper>();
+        
+        protected override void OnLoad(EventArgs e)
+        {
+            base.OnLoad(e);
+
+            var mediaTypes = Enum.GetValues(typeof(MediaType)).Cast<MediaType>().ToList();
+
+            var rows = new List<TableRow>();
+
+            foreach (var type in mediaTypes)
+            {
+                var stepper = new NumericStepper
+                {
+                    MinValue = 0,
+                    Width = 100,
+                };
+                
+                stepper.ValueChanged += StepperOnValueChanged;
+                
+                steppers.Add(type, stepper);
+                
+                rows.Add(new TableRow(
+                    type.GetDescription(),
+                    stepper,
+                    null
+                ));
+            }
+            
+            Content = new TableLayout(rows)
+            {
+                Spacing = new Size(5,5),
+            };
+        }
+
+        private void StepperOnValueChanged(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
--- Catalog/Catalog/Model/MediaType.cs
+++ Catalog/Catalog/Model/MediaType.cs
@@ -1,14 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Text;
 
 namespace Catalog.Model
 {
     public enum MediaType : byte
     {
+        [Description("5.25\" Floppy")]
         Floppy525,
+        [Description("3.5\" Floppy")]
         Floppy35,
-        CDROM,
-        DVDROM,
+        [Description("CD-ROM")]
+        CdRom,
+        [Description("DVD-ROM")]
+        DvdRom,
     }
 }
