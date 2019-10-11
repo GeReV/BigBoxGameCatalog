3a85b4038457d567b32d917ea6ff20097214fe61
Amir Grozki <amirgrozki@gmail.com>
2019-10-11 07:09:00 +0300

    Minor change.

--- Catalog/Catalog/Model/ItemTypes.cs
+++ Catalog/Catalog/Model/ItemTypes.cs
@@ -4,21 +4,21 @@ namespace Catalog.Model
 {
     public static class ItemTypes
     {
-        public static ItemType BigBox = new ItemType("bigbox", "Big Box"/*, new ImageSource(Icons.box)*/);
-        public static ItemType JewelCase = new ItemType("jewelcase", "Jewel Case"/*, new Bitmap(Icons.disc_case) */);
-        public static ItemType Manual = new ItemType("manual", "Manual"/*, new Bitmap(Icons.book) */);
+        public static ItemType BigBox = new ItemType("bigbox", "Big Box");
+        public static ItemType JewelCase = new ItemType("jewelcase", "Jewel Case");
+        public static ItemType Manual = new ItemType("manual", "Manual");
 
         [Category("Media")]
-        public static ItemType Floppy35 = new ItemType("floppy35", "3.5\" Floppy"/*, new Bitmap(Icons.disk) */);
+        public static ItemType Floppy35 = new ItemType("floppy35", "3.5\" Floppy");
         [Category("Media")]
-        public static ItemType Floppy525 = new ItemType("floppy525", "5.25\" Floppy"/*, new Bitmap(Icons.disk_black) */);
+        public static ItemType Floppy525 = new ItemType("floppy525", "5.25\" Floppy");
         [Category("Media")]
-        public static ItemType CdRom = new ItemType("cdrom", "CD-ROM"/*, new Bitmap(Icons.disc) */);
+        public static ItemType CdRom = new ItemType("cdrom", "CD-ROM");
         [Category("Media")]
-        public static ItemType DvdRom = new ItemType("dvdrom", "DVD-ROM"/*, new Bitmap(Icons.disc_blue) */);
+        public static ItemType DvdRom = new ItemType("dvdrom", "DVD-ROM");
 
-        public static ItemType Cassette = new ItemType("cassette", "Cassette"/*, new Bitmap(Icons.cassette) */);
-        public static ItemType Appendix = new ItemType("appendix", "Appendix"/*, new Bitmap(Icons.document) */);
+        public static ItemType Cassette = new ItemType("cassette", "Cassette");
+        public static ItemType Appendix = new ItemType("appendix", "Appendix");
 
         public static ItemType[] All = {
             BigBox,
