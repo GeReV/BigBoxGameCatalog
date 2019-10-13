bf5b144358388a97293cab87b239b0cd80f6ad91
Amir Grozki <amirgrozki@gmail.com>
2019-10-13 05:21:50 +0300

    Added highlighting to search.

---
+++ Catalog.Wpf/Forms/Controls/Highlight.cs
@@ -0,0 +1,31 @@
+﻿using System.Windows;
+using System.Windows.Documents;
+using Catalog.Wpf.Annotations;
+
+namespace Catalog.Wpf.Forms.Controls
+{
+    public class Highlight : Span
+    {
+        static Highlight()
+        {
+            FrameworkContentElement.DefaultStyleKeyProperty.OverrideMetadata(typeof(Highlight),
+                (PropertyMetadata) new FrameworkPropertyMetadata((object) typeof(Highlight)));
+        }
+
+        public Highlight()
+        {
+        }
+
+        public Highlight(Inline childInline) : base(childInline)
+        {
+        }
+
+        public Highlight(Inline childInline, TextPointer insertionPosition) : base(childInline, insertionPosition)
+        {
+        }
+
+        public Highlight([NotNull] TextPointer start, [NotNull] TextPointer end) : base(start, end)
+        {
+        }
+    }
+}
---
+++ Catalog.Wpf/Forms/Controls/HighlightedTextBlock.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Text.RegularExpressions;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+
+namespace Catalog.Wpf.Forms.Controls
+{
+    public partial class HighlightedTextBlock : TextBlock
+    {
+        public static readonly DependencyProperty HighlightedTextProperty = DependencyProperty.Register(
+            "HighlightedText", typeof(string), typeof(HighlightedTextBlock),
+            (PropertyMetadata) new UIPropertyMetadata(new PropertyChangedCallback(OnHighlightChanged)));
+
+        public string HighlightedText
+        {
+            get => (string) GetValue(HighlightedTextProperty);
+            set => SetValue(HighlightedTextProperty, value);
+        }
+
+        static HighlightedTextBlock()
+        {
+            FrameworkElement.DefaultStyleKeyProperty.OverrideMetadata(typeof(HighlightedTextBlock),
+                (PropertyMetadata) new FrameworkPropertyMetadata((object) typeof(HighlightedTextBlock)));
+        }
+
+        private static void OnHighlightChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            var textBlock = (TextBlock) d;
+
+            UpdateText(textBlock, (string) textBlock.GetValue(TextProperty), (string) e.NewValue);
+        }
+
+        private static void UpdateText(TextBlock textBlock, string text, string highlight)
+        {
+            if (string.IsNullOrWhiteSpace(highlight))
+            {
+                textBlock.Text = text;
+
+                return;
+            }
+
+            var r = new Regex(highlight, RegexOptions.Compiled | RegexOptions.IgnoreCase);
+
+            textBlock.Inlines.Clear();
+
+            var index = 0;
+
+            foreach (Match match in r.Matches(text))
+            {
+                textBlock.Inlines.Add(new Run(text.Substring(index, match.Index - index)));
+                textBlock.Inlines.Add(new Highlight(new Run(match.Value)));
+
+                index = match.Index + match.Length;
+            }
+
+            if (index < text.Length)
+            {
+                textBlock.Inlines.Add(new Run(text.Substring(index)));
+            }
+        }
+    }
+}
