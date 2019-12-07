3c6c466744bfe094e2a041d46775b379df91922c
Amir Grozki <amirgrozki@gmail.com>
2019-12-08 00:44:08 +0200

    Initial work on adding checkmark in context menu.

---
+++ Catalog.Wpf/Converters/ListIncludesToBooleanConverter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Windows;
+using System.Windows.Data;
+using Catalog.Wpf.ViewModel;
+
+namespace Catalog.Wpf.Converters
+{
+    public class ListIncludesToBooleanConverter : IMultiValueConverter
+    {
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (!(values[1] is IList list))
+            {
+                return DependencyProperty.UnsetValue;
+            }
+
+            return list.Contains(values[0]);
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
