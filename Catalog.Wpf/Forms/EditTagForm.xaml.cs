2f6a74954837d9c550676e5f6a83dda5be98a861
Amir Grozki <amirgrozki@gmail.com>
2019-12-16 01:02:47 +0200

    Renamed a few things.

---
+++ Catalog.Wpf/Forms/EditTagForm.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using Catalog.Wpf.ViewModel;
+
+namespace Catalog.Wpf.Forms
+{
+    public partial class EditTagForm : UserControl
+    {
+        public static readonly DependencyProperty GameTagProperty = DependencyProperty.Register(
+            nameof(GameTag), typeof(TagViewModel), typeof(EditTagForm), new PropertyMetadata(default(TagViewModel)));
+
+        public TagViewModel GameTag
+        {
+            get => (TagViewModel) GetValue(GameTagProperty);
+            set => SetValue(GameTagProperty, value);
+        }
+
+        public EditTagForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
