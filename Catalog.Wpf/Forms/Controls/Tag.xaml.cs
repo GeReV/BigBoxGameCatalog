63524f291ba8c4e4c3fb66adad45dba7fc3da014
Amir Grozki <amirgrozki@gmail.com>
2019-12-16 02:08:07 +0200

    Now changing label text color based on background luminance.

--- Catalog.Wpf/Forms/Controls/Tag.xaml.cs
+++ Catalog.Wpf/Forms/Controls/Tag.xaml.cs
@@ -11,7 +11,24 @@ namespace Catalog.Wpf.Forms.Controls
             nameof(Text), typeof(string), typeof(Tag), new PropertyMetadata(default(string)));
 
         public static readonly DependencyProperty ColorProperty = DependencyProperty.Register(
-            nameof(Color), typeof(Color), typeof(Tag), new PropertyMetadata(default(Color)));
+            nameof(Color), typeof(Color), typeof(Tag), new PropertyMetadata(
+                Color.DarkGray,
+                null,
+                ColorCoerceValueCallback
+            ));
+
+        public static readonly DependencyProperty TextColorProperty = DependencyProperty.Register(
+            nameof(TextColor), typeof(Color), typeof(Tag), new PropertyMetadata(default(Color)));
+
+        private static object ColorCoerceValueCallback(DependencyObject d, object basevalue)
+        {
+            var color = (Color) basevalue;
+            var luminance = (0.299f * color.R + 0.587f * color.G + 0.114f * color.B) / 255f;
+
+            d.SetValue(TextColorProperty, luminance > 0.5 ? Color.Black : Color.White);
+
+            return color.ToArgb() == 0 ? Color.DarkGray : color;
+        }
 
         public Tag()
         {
@@ -27,7 +44,13 @@ namespace Catalog.Wpf.Forms.Controls
         public Color Color
         {
             get => (Color) GetValue(ColorProperty);
-            set => SetValue(ColorProperty, value.IsEmpty ? Color.LightGray : value);
+            set => SetValue(ColorProperty, value);
+        }
+
+        public Color TextColor
+        {
+            get => (Color) GetValue(TextColorProperty);
+            set => SetValue(TextColorProperty, value);
         }
     }
 }
