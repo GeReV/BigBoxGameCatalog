53f0e5b094cfff62b59916133606ad2ccdaf6b59
Amir Grozki <amirgrozki@gmail.com>
2019-12-22 09:36:05 +0200

    Will now show tag checkmark only when all selected games have the tag.

--- Catalog.Wpf/Converters/ListIncludesToBooleanConverter.cs
+++ Catalog.Wpf/Converters/ListIncludesToBooleanConverter.cs
@@ -1,13 +1,10 @@
 ﻿using System;
 using System.Collections;
-using System.Collections.Generic;
 using System.Globalization;
 using System.Linq;
 using System.Windows;
 using System.Windows.Data;
 using Catalog.Model;
-using Catalog.Wpf.ViewModel;
-using Microsoft.EntityFrameworkCore;
 
 namespace Catalog.Wpf.Converters
 {
@@ -15,17 +12,34 @@ namespace Catalog.Wpf.Converters
     {
         public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
         {
+            if (!(values[0] is IModel model))
+            {
+                return DependencyProperty.UnsetValue;
+            }
+
             if (!(values[1] is IList list))
             {
                 return DependencyProperty.UnsetValue;
             }
 
-            if (values[0] is IModel model)
+            if (list.OfType<IModel>().Count() == list.Count)
             {
                 return list
                     .OfType<IModel>()
                     .ToList()
                     .Exists(item => model.GetType() == item.GetType() && item.Id == model.Id);
+            }
+
+            if (list.OfType<IList>().Count() == list.Count)
+            {
+                return list
+                    .OfType<IList>()
+                    .All(sublist =>
+                        sublist
+                            .OfType<IModel>()
+                            .ToList()
+                            .Exists(item => model.GetType() == item.GetType() && item.Id == model.Id)
+                    );
             }
 
             return list.Contains(values[0]);
---
+++ Catalog.Wpf/Converters/PropertyPathConverter.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Windows;
+using System.Windows.Data;
+
+namespace Catalog.Wpf.Converters
+{
+    public class PropertyPathConverter : IValueConverter
+    {
+        public object Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+        {
+            if (parameter == null)
+            {
+                return value;
+            }
+
+            var properties = parameter.ToString().Split('.', StringSplitOptions.RemoveEmptyEntries);
+
+            if (value is IList list)
+            {
+                var result = new ArrayList(list.Count);
+
+                foreach (var obj in list)
+                {
+                    result.Add(TraversePath(obj, properties));
+                }
+
+                return result;
+            }
+
+            return TraversePath(value, properties) ?? DependencyProperty.UnsetValue;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+
+        private static object TraversePath(object? value, IEnumerable<string> properties)
+        {
+            var result = value;
+
+            foreach (var propertyName in properties)
+            {
+                if (result == null)
+                {
+                    return DependencyProperty.UnsetValue;
+                }
+
+                var property = result.GetType().GetProperty(propertyName);
+
+                if (property == null)
+                {
+                    return DependencyProperty.UnsetValue;
+                }
+
+                result = property.GetValue(result);
+            }
+
+            return result;
+        }
+    }
+}
