71189c8e8d158f468a73c367bce3f647f57aaa20
Amir Grozki <amirgrozki@gmail.com>
2019-12-24 03:22:26 +0200

    Added AsyncDelegateCommand.

---
+++ Catalog.Wpf/Commands/AsyncDelegateCommand.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Catalog.Wpf.Commands
+{
+    public class AsyncDelegateCommand : AsyncCommandBase
+    {
+        private readonly Func<object?, Task> execute;
+        private readonly Predicate<object?>? canExecute;
+
+        public AsyncDelegateCommand(Func<object?, Task> execute, Predicate<object>? canExecute = null)
+        {
+            this.execute = execute;
+            this.canExecute = canExecute;
+        }
+
+        protected override bool CanExecuteImpl(object? parameter) => canExecute?.Invoke(parameter) ?? true;
+
+        protected override Task Perform(object? parameter) => execute.Invoke(parameter);
+    }
+}
