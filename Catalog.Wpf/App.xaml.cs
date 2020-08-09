3093b9c17276fc8b4b36a09a8b3c7aeed34c051e
Amir Grozki <amirgrozki@gmail.com>
2020-08-09 05:08:19 +0300

    Added database backup functionality and bumped version to 0.3.0.

--- Catalog.Wpf/App.xaml.cs
+++ Catalog.Wpf/App.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Globalization;
 using System.IO;
+using System.Linq;
 using System.Reflection;
 using System.Windows;
 using Catalog.Scrapers;
@@ -13,6 +15,11 @@ namespace Catalog.Wpf
     public partial class App
     {
         private const string FALLBACK_DIRECTORY_NAME = "BBGC";
+
+        private const string BACKUP_DIRECTORY = "backups";
+        private const string BACKUP_FILENAME_PATTERN = "*.sqlite";
+        private const string BACKUP_FILENAME_DATE_PATTERN = "yyyyMMddhhmmss";
+        private readonly TimeSpan BACKUP_AGE_THRESHOLD = TimeSpan.FromDays(7);
 
         public App()
         {
@@ -29,7 +36,8 @@ namespace Catalog.Wpf
             var settingsDirectory = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData);
 
             var homeDirectory = Path.Combine(settingsDirectory,
-                Assembly.GetExecutingAssembly().GetCustomAttribute<AssemblyTitleAttribute>()?.Title ?? FALLBACK_DIRECTORY_NAME);
+                Assembly.GetExecutingAssembly().GetCustomAttribute<AssemblyTitleAttribute>()?.Title ??
+                FALLBACK_DIRECTORY_NAME);
 
             if (!Directory.Exists(homeDirectory))
             {
@@ -39,9 +47,58 @@ namespace Catalog.Wpf
             return homeDirectory;
         }
 
-        private static void InitializeDatabase()
+        private static DateTime? ParseDateTimeFromFilename(FileSystemInfo info)
+        {
+            var basename = info.Name.Replace(info.Extension, string.Empty);
+
+            if (DateTime.TryParseExact(basename, BACKUP_FILENAME_DATE_PATTERN, null,
+                DateTimeStyles.None, out var result))
+            {
+                return result;
+            }
+
+            return null;
+        }
+
+        private void BackupDatabase(DbContext context)
+        {
+            context.Database.OpenConnection();
+
+            if (context.Database.IsSqlite())
+            {
+                var connection = context.Database.GetDbConnection();
+                var sourceFileInfo = new FileInfo(connection.DataSource);
+
+                var backupDirectory = sourceFileInfo.Directory.CreateSubdirectory(BACKUP_DIRECTORY);
+
+                var latestBackup = backupDirectory
+                    .EnumerateFiles(BACKUP_FILENAME_PATTERN)
+                    .OrderByDescending(info => info.CreationTime)
+                    .FirstOrDefault();
+
+                if (latestBackup == null ||
+                    sourceFileInfo.LastWriteTime > latestBackup.CreationTime &&
+                    DateTime.Now.Subtract(latestBackup.CreationTime) > BACKUP_AGE_THRESHOLD)
+                {
+                    MakeDatabaseBackup(sourceFileInfo, backupDirectory);
+                }
+            }
+
+            context.Database.CloseConnection();
+        }
+
+        private static void MakeDatabaseBackup(FileInfo sourceFileInfo, FileSystemInfo backupDirectory)
+        {
+            var backupFilename = $"{DateTime.Now.ToString(BACKUP_FILENAME_DATE_PATTERN)}.sqlite";
+
+            sourceFileInfo.CopyTo(Path.Join(backupDirectory.FullName, backupFilename));
+        }
+
+        private void InitializeDatabase()
         {
             var context = Current.Database();
+
+            BackupDatabase(context);
 
             context.Database.Migrate();
             context.Database.EnsureCreated();
