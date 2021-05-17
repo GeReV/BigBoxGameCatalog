07ded8826fad0e03a2464a9f880c507a9781f6b1
Amir Grozki <amirgrozki@gmail.com>
2021-05-17 07:31:27 +0300

    Configuration changes.

--- publish.bat
+++ publish.bat
@@ -1,12 +1,16 @@
+@REM Use Publish Catalog.Wpf configuration from IDE.
+@REM Figure out why published package didn't run.
+
 pushd Catalog.Wpf
 
 rd "bin/Release" /S /Q
 
-dotnet publish -r win10-x64 -c Release
+dotnet publish -r win-x64 -c Release
 
-pushd bin/Release
+pushd "bin/Release"
 
 del /Q /F /S "*.pdb"
+del /Q /F /S "*.md"
 
 popd
 
