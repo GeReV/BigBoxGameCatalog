823d238eb25f1ad2e92f5e2eed4c1cea18dc8582
Amir Grozki <amirgrozki@gmail.com>
2023-03-14 12:37:53 +0200

    Added Windows (no version) platform.

--- Catalog/Model/Platform.cs
+++ Catalog/Model/Platform.cs
@@ -4,17 +4,12 @@ namespace Catalog.Model
 {
     public enum Platform
     {
-        [Description("DOS")]
-        Dos = 1,
-        [Description("Windows 3.11")]
-        Win311,
-        [Description("Windows 95")]
-        Win95,
-        [Description("Windows 98")]
-        Win98,
-        [Description("Windows XP")]
-        WinXp,
-        [Description("Windows 7 or higher")]
-        Win7OrHigher,
+        [Description("DOS")] Dos = 1,
+        [Description("Windows")] Windows,
+        [Description("Windows 3.11")] Win311,
+        [Description("Windows 95")] Win95,
+        [Description("Windows 98")] Win98,
+        [Description("Windows XP")] WinXp,
+        [Description("Windows 7 or higher")] Win7OrHigher,
     }
 }
