a402d0174b3c2891c0384e0e1ab4b787884d942e
Amir Grozki <amirgrozki@gmail.com>
2023-03-14 12:07:33 +0200

    Updated MobyGames Rating object properties.

--- MobyGames.API/DataObjects/Rating.cs
+++ MobyGames.API/DataObjects/Rating.cs
@@ -1,5 +1,15 @@
-﻿namespace MobyGames.API.DataObjects;
+﻿using System.Text.Json.Serialization;
+
+namespace MobyGames.API.DataObjects;
 
 public class Rating
 {
+    [JsonPropertyName("rating_id")] public uint Id { get; set; }
+
+    [JsonPropertyName("rating_name")] public string Name { get; set; }
+
+    [JsonPropertyName("rating_system_id")] public uint RatingSystemId { get; set; }
+
+    [JsonPropertyName("rating_system_name")]
+    public string RatingSystemName { get; set; }
 }
